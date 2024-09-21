56c410fb6f6713d408010a3a320c0c3b3d5cd48b
Amrit <amritsinghal2211@gmail.com>
2024-09-21 18:27:28 +0530

    auth screen ui done

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -3,16 +3,21 @@ import React ,{useState} from 'react'
 import {images} from '../../constants'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import FormField from '../../components/FormField'
+import CustomButton from '../../components/CustomButton'
+import { Link } from 'expo-router'
 const SignIn = () => {
-
+  const [isSubmitting, setIsSubmitting] = useState(false)
   const [form, setForm] = useState({
     email: "",
     password:""
   })
+  const submit = ()=>{
+    
+  }
   return (
     <SafeAreaView className = "h-full bg-primary">
       <ScrollView>
-        <View className="justify-center w-full h-full px-4 my-6 h">
+        <View className="justify-center w-full  px-4 my-6  min-h-[83vh]">
           <Image source={images.logo} resizeMode='contain' className="w-[115px] h-[35px]"/>
           <Text className="mt-10 text-2xl font-semibold text-white font-psemibold">
             Log in to Aora
@@ -30,6 +35,23 @@ const SignIn = () => {
             handleChangeText={(e)=>setForm({...form,password:e})}
             otherStyles= "mt-7" 
           />
+        <CustomButton
+         title="Sign In"
+         handlePress={submit}
+         containerStyles="mt-7"
+         isLoading={isSubmitting}
+        />
+        <View className="flex-row justify-center gap-2 pt-5">
+          <Text className="text-lg text-gray-100 font-pregular">
+            Don't have account?
+          </Text>
+          <Link
+              href="/sign-up"
+              className="text-lg font-psemibold text-secondary"
+            >
+              Signup
+            </Link>
+        </View>
         </View>
       </ScrollView>
     </SafeAreaView>
--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,11 +1,67 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-
+import { View, Text, ScrollView, Image } from 'react-native'
+import React ,{useState} from 'react'
+import {images} from '../../constants'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import FormField from '../../components/FormField'
+import CustomButton from '../../components/CustomButton'
+import { Link } from 'expo-router'
 const SignUp = () => {
+  const [isSubmitting, setIsSubmitting] = useState(false)
+  const [form, setForm] = useState({
+    username: "",
+    email: "",
+    password:""
+  })
+  const submit = ()=>{
+    
+  }
   return (
-    <View>
-      <Text>SignUp</Text>
-    </View>
+    <SafeAreaView className = "h-full bg-primary">
+      <ScrollView>
+        <View className="justify-center w-full  px-4 my-6  min-h-[83vh]">
+          <Image source={images.logo} resizeMode='contain' className="w-[115px] h-[35px]"/>
+          <Text className="mt-10 text-2xl font-semibold text-white font-psemibold">
+            Sign up to Aora
+          </Text>
+          <FormField
+            title = "Username"
+            value = {form.username}
+            handleChangeText={(e)=>setForm({...form,username:e})}
+            otherStyles= "mt-10"
+          />
+          <FormField
+            title = "Email"
+            value = {form.email}
+            handleChangeText={(e)=>setForm({...form,email:e})}
+            otherStyles= "mt-7"
+            keyboardType = "email-address"
+          />
+          <FormField
+            title = "Password"
+            value = {form.password}
+            handleChangeText={(e)=>setForm({...form,password:e})}
+            otherStyles= "mt-7" 
+          />
+        <CustomButton
+         title="Sign In"
+         handlePress={submit}
+         containerStyles="mt-7"
+         isLoading={isSubmitting}
+        />
+        <View className="flex-row justify-center gap-2 pt-5">
+          <Text className="text-lg text-gray-100 font-pregular">
+            Already have an account?
+          </Text>
+          <Link
+              href="/sign-in"
+              className="text-lg font-psemibold text-secondary"
+            >
+              Signin
+            </Link>
+        </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
